7eda0cf3f0e730b435720f6fd63b2db6f6b48e9d
Kevin <kevinkwan1997@gmail.com>
2022-09-21 17:28:58 -0400

    remove component repo

--- cyberduck-components/src/components/buttons/button.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ButtonStyleParams {
-    height: string;
-    width: string;
-    backgroundColor: string;
-    textColor: string;
-    border: string;
-    boxShadow: string;
-    textShadow: string;
-}
